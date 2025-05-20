c7716c57d4d8e7cd0429e58a1d94167d736360ab
Tim Daubenschütz <tim@daubenschuetz.de>
2025-05-20 18:06:06 +0200

    progress

--- check.sh
+++ check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Deployed contract addresses
-CONTRACT_A="0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
-CONTRACT_B="0x5FbDB2315678afecb367f032d93F642f64180aa3"
+CONTRACT_A="0x0165878A594ca255338adfa4d48449f69242Eb8F"
+CONTRACT_B="0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
 # Check ball on chain A (901)
 echo "Checking ball on OPChainA (901):"
--- hitback.sh
+++ hitback.sh
@@ -5,8 +5,8 @@ CONTRACT_A="0x0165878A594ca255338adfa4d48449f69242Eb8F"
 CONTRACT_B="0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 
-# Hit ball from Chain A (901) to Chain B (902)
-echo "Hitting ball from Chain A to Chain B:"
-cast send $CONTRACT_A "hitBallTo(uint256)" 902 \
-  --rpc-url http://127.0.0.1:9545 \
+# Hit ball from Chain B (902) to Chain A (901)
+echo "Hitting ball from Chain B to Chain A:"
+cast send $CONTRACT_B "hitBallTo(uint256)" 901 \
+  --rpc-url http://127.0.0.1:9546 \
   --private-key $PRIVATE_KEY
